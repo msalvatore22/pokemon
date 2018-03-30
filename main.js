e27bd49bee6fb053d380f94ba3200cf6c91e585c
Michael Salvatore <msalvatore22@gmail.com>
2018-03-30 15:42:30 -0400

    displaying all data except abilities from api

--- main.js
+++ main.js
@@ -28,33 +28,147 @@ class Pokemon {
 	// }
 }
 
-axios.get("https://pokeapi.co/api/v2/pokemon/146").then((response) => {
+axios.get("https://pokeapi.co/api/v2/pokemon/146/").then((response) => {
 	let data = response.data
-	let name = response.data.name
-	let hp = response.data.stats[5].base_stat
 	let trainer = new Trainer()
 	let section = document.querySelector("#moltres")
-	let ul = document.createElement("ul")
-	let li = document.createElement("li")
+	let name = document.querySelector(".moltres-name")
+	let hp = document.querySelector(".moltres-hp")
+	let hpValue = document.querySelector(".moltres-hp-value")
+	let attack = document.querySelector(".moltres-attack")
+	let attackValue = document.querySelector(".moltres-attack-value")
+	let defense = document.querySelector(".moltres-defense")
+	let defenseValue = document.querySelector(".moltres-defense-value")
+	let abilitiesFirst = document.querySelector(".moltres-abilities1")
+	let abilitiesSecond = document.querySelector(".moltres-abilities2")
+
 
 	let moltres = new Pokemon (
-			data.name
+			data.name,
+			data.stats[5].stat.name,
+			data.stats[5].base_stat,
+			data.stats[4].stat.name,
+			data.stats[4].base_stat,
+			data.stats[3].stat.name,
+			data.stats[3].base_stat,
+			data.abilities[0].ability.name,
+			data.abilities[1].ability.name
 
 		)
 
 
 		trainer.add(moltres)
 
-		li.innerHTML = moltres.name
-
-		section.appendChild(ul)
-		ul.appendChild(li)
+		name.innerHTML = data.name
+		hp.innerHTML = data.stats[5].stat.name
+		hpValue.innerHTML = data.stats[5].base_stat
+		attack.innerHTML = data.stats[4].stat.name
+		attackValue.innerHTML = data.stats[4].base_stat
+		defense.innerHTML = data.stats[3].stat.name
+		defenseValue.innerHTML = data.stats[3].base_stat
+		abilitiesFirst.innerHTML = data.abilities[0].ability.name
+		abilitiesSecond.innerHTML = data.abilities[1].ability.name
+ 
 
 
 }).catch((error) => {
   console.log(error)
 })
 
+axios.get("https://pokeapi.co/api/v2/pokemon/245/").then((response) => {
+	let data = response.data
+	let trainer = new Trainer()
+	let section = document.querySelector("#suicune")
+	let name = document.querySelector(".suicune-name")
+	let hp = document.querySelector(".suicune-hp")
+	let hpValue = document.querySelector(".suicune-hp-value")
+	let attack = document.querySelector(".suicune-attack")
+	let attackValue = document.querySelector(".suicune-attack-value")
+	let defense = document.querySelector(".suicune-defense")
+	let defenseValue = document.querySelector(".suicune-defense-value")
+	let abilitiesFirst = document.querySelector(".suicune-abilities1")
+	let abilitiesSecond = document.querySelector(".suicune-abilities2")
+
+
+	let suicune = new Pokemon (
+			data.name,
+			data.stats[5].stat.name,
+			data.stats[5].base_stat,
+			data.stats[4].stat.name,
+			data.stats[4].base_stat,
+			data.stats[3].stat.name,
+			data.stats[3].base_stat,
+			data.abilities[0].ability.name,
+			data.abilities[1].ability.name
+
+		)
+
+
+		trainer.add(suicune)
+
+		name.innerHTML = data.name
+		hp.innerHTML = data.stats[5].stat.name
+		hpValue.innerHTML = data.stats[5].base_stat
+		attack.innerHTML = data.stats[4].stat.name
+		attackValue.innerHTML = data.stats[4].base_stat
+		defense.innerHTML = data.stats[3].stat.name
+		defenseValue.innerHTML = data.stats[3].base_stat
+		abilitiesFirst.innerHTML = data.abilities[0].ability.name
+		abilitiesSecond.innerHTML = data.abilities[1].ability.name
+ 
+
+
+}).catch((error) => {
+  console.log(error)
+})
+
+
+axios.get("https://pokeapi.co/api/v2/pokemon/250/").then((response) => {
+	let data = response.data
+	let trainer = new Trainer()
+	let section = document.querySelector("#ho-oh")
+	let name = document.querySelector(".ho-oh-name")
+	let hp = document.querySelector(".ho-oh-hp")
+	let hpValue = document.querySelector(".ho-oh-hp-value")
+	let attack = document.querySelector(".ho-oh-attack")
+	let attackValue = document.querySelector(".ho-oh-attack-value")
+	let defense = document.querySelector(".ho-oh-defense")
+	let defenseValue = document.querySelector(".ho-oh-defense-value")
+	let abilitiesFirst = document.querySelector(".ho-oh-abilities1")
+	let abilitiesSecond = document.querySelector(".ho-oh-abilities2")
+
+
+	let hoOh = new Pokemon (
+			data.name,
+			data.stats[5].stat.name,
+			data.stats[5].base_stat,
+			data.stats[4].stat.name,
+			data.stats[4].base_stat,
+			data.stats[3].stat.name,
+			data.stats[3].base_stat,
+			data.abilities[0].ability.name,
+			data.abilities[1].ability.name
+
+		)
+
+
+		trainer.add(hoOh)
+
+		name.innerHTML = data.name
+		hp.innerHTML = data.stats[5].stat.name
+		hpValue.innerHTML = data.stats[5].base_stat
+		attack.innerHTML = data.stats[4].stat.name
+		attackValue.innerHTML = data.stats[4].base_stat
+		defense.innerHTML = data.stats[3].stat.name
+		defenseValue.innerHTML = data.stats[3].base_stat
+		abilitiesFirst.innerHTML = data.abilities[0].ability.name
+		abilitiesSecond.innerHTML = data.abilities[1].ability.name
+ 
+
+
+}).catch((error) => {
+  console.log(error)
+})
 // axios.get("https://pokeapi.co/api/v2/pokemon/245").then((response) => {
 // 	let data = response.data
 // 	let trainer = new Trainer()
